e57fecae987b92bb04f67c6443787cd2f19a55ec
Lucas Gabriel Pereira Santos <lucasgabrieliftm@gmail.com>
2020-06-04 15:09:57 -0300

    Path settings.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 const express = require("express")
 const server = express()
 
+
+//configuring the public folder
+server.use(express.static("public"))
+
 // Setting appltication paths
-
 // inicial page
-
 //req: Requisition
 //res: Answer
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
 
 
 //ligar o servidor
