069acf1f9075b0a53a855e68630355cd2ade1623
Lucas Gabriel Pereira Santos <lucasgabrieliftm@gmail.com>
2020-06-04 13:11:00 -0300

    Server settings

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const server = express()
+
+//configurar caminhos da aplicação
+
+// página inicial
+
+server.get("/", (req, res) => {
+
+})
+
+
+
+//ligar o servidor
+server.listen(3000)
